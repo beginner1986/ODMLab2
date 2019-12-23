5b921a72023c1b1b1f24f02daef8ea466c09baf7
Adam Emieljaniuk <beginner1986@gmail.com>
2019-12-23 19:20:09 +0100

    Project structure changed.

--- ODMLab2/ODMLab2/Main.cpp
+++ ODMLab2/ODMLab2/Main.cpp
@@ -1,16 +1,9 @@
-#include <iostream>
-#include "D.h"
+#include "Task1.cpp"
 
 int main()
 {
-	// zadanie 1
-	A* a = new A(1, 2);
-	B* b = new B(2, 4, 6, 8);
-	C* c = new C(3, 6, 9, 12, 15, 18);
-	D* d = new D(1, 2, 3, 4, 5, 6, 7, 8);
+	Task1 t1;
+	t1.Start();
 
-	delete a;
-	delete b;
-	delete c;
-	delete d;
+	return 0;
 }
---
+++ ODMLab2/ODMLab2/Task1/Task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "D.h"
+
+class Task1
+{
+public:
+	int Start()
+	{
+		A* a = new A(1, 2);
+		B* b = new B(2, 4, 6, 8);
+		C* c = new C(3, 6, 9, 12, 15, 18);
+		D* d = new D(1, 2, 3, 4, 5, 6, 7, 8);
+
+		delete a;
+		delete b;
+		delete c;
+		delete d;
+	}
+};
