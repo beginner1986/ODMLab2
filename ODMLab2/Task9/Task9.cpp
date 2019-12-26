8dda6850a5876a86e02a96f93114c7f0cb1721e4
Adam Emieljaniuk <beginner1986@gmail.com>
2019-12-26 21:27:10 +0100

    Task 9 reorganized.

--- ODMLab2/Task9/Task9.cpp
+++ ODMLab2/Task9/Task9.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
 #include <functional>
 
+// TASK 9: lambda expressions
+
 int main()
 {
-	std::function<int(int, int&)> f = 
+	// lambda expression
+	std::cout << "lambda expression" << std::endl;
+	int x = 3, y = 5;
+	std::cout << "x = " << x << ", y = " << y << std::endl;
+	
+	auto lambda = [](int x, int& y) {
+		int result = x + y;
+		y = 0;
+		return result;;
+	};
+	
+	std::cout << "lambda = " << lambda(x, y) << std::endl;
+	std::cout << "x = " << x << ", y = " << y << std::endl << std::endl;
+
+	// function
+	std::cout << "std::function<int(int, int&)>" << std::endl;
+	x = 3, y = 5;
+	std::cout << "x = " << x << ", y = " << y << std::endl;
+
+	std::function<int(int, int&)> f =
 		[](int x, int& y) {
 		int result = x + y;
 		y = 0;
 		return result;
 	};
 
-	std::cout << "lambda expression" << std::endl;
-	int x = 3, y = 5;
-	std::cout << "x = " << x << ", y = " << y << std::endl;
-	auto lambda = [](int x, int& y) {
-		int result = x + y;
-		y = 0;
-		return result;;
-	};
-	std::cout << "lambda = " << lambda(x, y) << std::endl;
-	std::cout << "x = " << x << ", y = " << y << std::endl << std::endl;
-
-	std::cout << "std::function<int(int, int&)>" << std::endl;
-	x = 3, y = 5;
-	std::cout << "x = " << x << ", y = " << y << std::endl;
 	std::cout << "f = " << f(x, y) << std::endl;
 	std::cout << "x = " << x << ", y = " << y << std::endl;
 
