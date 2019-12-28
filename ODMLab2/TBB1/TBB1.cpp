6f2f5fffb025dd960ece85285100f20900d12fd7
Adam Emieljaniuk <beginner1986@gmail.com>
2019-12-28 16:49:12 +0100

    TBB tasks 1 and 2 sequence versions and time measurement added.

--- ODMLab2/TBB1/TBB1.cpp
+++ ODMLab2/TBB1/TBB1.cpp
@@ -26,7 +26,10 @@ int main()
 	std::cout << "Functor object:" << std::endl;
 	Print print;
 
+	tbb::tick_count before = tbb::tick_count::now();
 	tbb::parallel_invoke(print, print);
+	tbb::tick_count after = tbb::tick_count::now();
+	std::cout << "time:" << (after - before).seconds() << "s" << std::endl;
 
 	// lambda
 	std::cout << "\nLambda:" << std::endl;
@@ -35,12 +38,26 @@ int main()
 			std::cout << "Hello World!\n"; 
 	};
 
+	before = tbb::tick_count::now();
 	tbb::parallel_invoke(lambda, lambda);
+	after = tbb::tick_count::now();
+	std::cout << "time:" << (after - before).seconds() << "s" << std::endl;
 
 	// function pointer
 	std::cout << "\nFunction pointer:" << std::endl;
 	const auto funcPtr = function;	// void(* const funcPtr)() = function;
+	
+	before = tbb::tick_count::now();
 	tbb::parallel_invoke(funcPtr, funcPtr);
+	after = tbb::tick_count::now();
+	std::cout << "time:" << (after - before).seconds() << "s" << std::endl;
+
+	// sequence
+	before = tbb::tick_count::now();
+	for (int i = 0; i < 16; i++)
+		std::cout << "Hello World!" << std::endl;
+	after = tbb::tick_count::now();
+	std::cout << "time:" << (after - before).seconds() << "s" << std::endl;
 
 	return 0;
 }
--- ODMLab2/TBB2/TBB2.cpp
+++ ODMLab2/TBB2/TBB2.cpp
@@ -7,6 +7,7 @@
 
 int main()
 {
+	// setup
 	std::random_device generator;
 	std::uniform_int_distribution<int> distribution(1, 1000000);
 
@@ -14,11 +15,25 @@ int main()
 	for (int i = 0; i < 100; i++)
 		vector.push_back(distribution(generator));
 
+	// tbb::parallel_for_each
+	tbb::tick_count before = tbb::tick_count::now();
 	tbb::parallel_for_each(vector.begin(), vector.end(), 
 		[&](int& i) {
-			std::cout << i << " ";
+			std::string print = std::to_string(i) + " ";
+			std::cout << print;
 	});
-	std::cout << std::endl;
+	tbb::tick_count after = tbb::tick_count::now();
+	std::cout << std::endl << "time: " << (after - before).seconds() << "s" 
+		<< std::endl << std::endl;;
+
+	// sequence
+	before = tbb::tick_count::now();
+	for (int i : vector)
+	{
+		std::cout << i << " ";
+	}
+	after = tbb::tick_count::now();
+	std::cout << std::endl << "time: " << (after - before).seconds() << "s" << std::endl;
 
 	return 0;
 }
